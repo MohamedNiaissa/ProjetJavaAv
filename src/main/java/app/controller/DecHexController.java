71c46186038ef19013d4ccaced39f6e3e5584042
CyrGeo <cyrille.george@gmail.com>
2021-12-08 16:33:31 +0100

    Adding the Decimal to Hexadecimal conversion back and forth (converter.java amended + DecHexController.java modified + Scene of the Story)

--- src/main/java/app/controller/DecHexController.java
+++ src/main/java/app/controller/DecHexController.java
@@ -1,9 +1,11 @@
 package app.controller;
 
 import app.model.Converter;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,18 +17,26 @@ public class DecHexController implements Initializable {
     @FXML
     private TextField txtHexadecimal;
 
+    @FXML
+    private TextField txtMessage;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        txtHexadecimal.selectionProperty().addListener(observable -> {
-            int test = Converter.hexToDec(txtHexadecimal.getText());
-            txtDecimal.setText(Integer.toString(test));
+
+        txtHexadecimal.setOnKeyReleased(event -> {
+            if (txtHexadecimal.getText().matches("(?i)[0-9-abcdef]+")) {
+                String myDec = Converter.hexToDec(txtHexadecimal.getText());
+                txtDecimal.setText(myDec);
+                txtMessage.setText("verified : " + txtHexadecimal.getText() + " <=> " + Integer.toString(Integer.parseInt(myDec)));
+            } else txtMessage.setText("Bad Number input: use only allowed chars.");
         });
 
-        txtDecimal.selectionProperty().addListener(observable -> {
-            String hexaValue = Converter.decToHex(txtDecimal.getText());
-            txtHexadecimal.setText(hexaValue);
+        txtDecimal.setOnKeyReleased(event -> {
+            if(txtDecimal.getText().matches(".*[0-9]$")){
+                String hexaValue = Converter.decToHex(txtDecimal.getText());
+                txtHexadecimal.setText(hexaValue);
+                txtMessage.setText("verified : " + txtDecimal.getText() + " <=> " + Integer.toHexString(Integer.parseInt(txtDecimal.getText())));
+            }else txtMessage.setText("Bad Number input: use only allowed chars.");
         });
-
     }
-
 }
--- src/main/java/app/model/Converter.java
+++ src/main/java/app/model/Converter.java
@@ -17,32 +17,31 @@ public class Converter {
         while(temp !=0){
             index = temp%16;
             temp = temp/16;
-            System.out.println("temp "+temp+" index "+index+" Char "+seize[index]);
             hexa=seize[index]+hexa;
-            System.out.println("hexa "+hexa);
         }
-        System.out.println("hexa= "+hexa);
         return hexa;
     }
 
-    public static int hexToDec(String hex){
+    public static String hexToDec(String hex){
         char[] seize = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
         int dec = 0;
         char[] hex2 = hex.toCharArray();
         int start = 0;
         int index = 0;
+        boolean badInput = false;
         if (hex2[0] == '#') start = 1;
         for (int i = hex2.length-1; i >= start; i--) {
             for (int j = 0; j < seize.length; j++) {
+                badInput = false;
                 if(hex2[i]==seize[j]){
                     index = j;
                     continue;
                 }
+            if(badInput) return "bxad";
             }
             dec += index * Math.pow(16, hex.length() - i-1);
         }
-        System.out.println("dec= "+dec);
-        return (int) dec;
+        return Integer.toString(dec);
     }
 
     public void DecToRom() {
