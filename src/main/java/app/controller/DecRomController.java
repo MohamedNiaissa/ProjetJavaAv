cb45124f7e0ee9ef657657bb8bfa540f2542eab4
Maghwyn <christopher.samson@edu.esiee-it.fr>
2021-12-08 13:21:38 +0100

    Implemented a brute and very fragile method to convert roman number to integer

--- src/main/java/app/controller/DecRomController.java
+++ src/main/java/app/controller/DecRomController.java
@@ -1,12 +1,28 @@
 package app.controller;
 
+import app.model.Converter;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DecRomController implements Initializable {
+    Converter converter = new Converter();
+
+    @FXML  private TextField TFDecimal;
+    @FXML  private TextField TFRomain;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        TFDecimal.textProperty().addListener(event -> {
+            String convert = converter.DecToRom(TFRomain.getText());
+            TFRomain.setText(String.valueOf(convert));
+        });
 
+        TFRomain.textProperty().addListener(event -> {
+            int convert = converter.RomToDec(TFRomain.getText());
+            TFDecimal.setText(String.valueOf(convert));
+        });
     }
 }
--- src/main/java/app/model/Converter.java
+++ src/main/java/app/model/Converter.java
@@ -14,10 +14,94 @@ public class Converter {
     public void HexToDec() {
     }
 
-    public void DecToRom() {
+    public String DecToRom(String text) {
+        return text;
     }
 
-    public void RomToDec() {
+    public int RomToDec(String strRomain) {
+        //It's a temporary brute force method.
+        int strLen = strRomain.length();
+        int converted = 0;
+        for(int nbChar = 0; nbChar < strLen; nbChar++) {
+            if(strRomain.charAt(nbChar) != strRomain.charAt(strLen - 1)) {
+                String specialCase = "" + strRomain.charAt(nbChar) + strRomain.charAt(nbChar + 1);
+
+                switch (specialCase) {
+                    case "IV" -> {
+                        converted += 4;
+                        nbChar++;
+                        continue;
+                    }
+                    case "IX" -> {
+                        converted += 9;
+                        nbChar++;
+                        continue;
+                    }
+                    case "XL" -> {
+                        converted += 40;
+                        nbChar++;
+                        continue;
+                    }
+                    case "IL" -> {
+                        converted += 49;
+                        nbChar++;
+                        continue;
+                    }
+                    case "XC" -> {
+                        converted += 90;
+                        nbChar++;
+                        continue;
+                    }
+                    case "IC" -> {
+                        converted += 99;
+                        nbChar++;
+                        continue;
+                    }
+                    case "CD" -> {
+                        converted += 400;
+                        nbChar++;
+                        continue;
+                    }
+                    case "XD" -> {
+                        converted += 490;
+                        nbChar++;
+                        continue;
+                    }
+                    case "ID" -> {
+                        converted += 499;
+                        nbChar++;
+                        continue;
+                    }
+                    case "CM" -> {
+                        converted += 900;
+                        nbChar++;
+                        continue;
+                    }
+                    case "XM" -> {
+                        converted += 990;
+                        nbChar++;
+                        continue;
+                    }
+                    case "IM" -> {
+                        converted += 999;
+                        nbChar++;
+                        continue;
+                    }
+                }
+            }
+
+            switch(strRomain.charAt(nbChar)) {
+                case 'I' -> converted += 1;
+                case 'V' -> converted += 5;
+                case 'X' -> converted += 10;
+                case 'L' -> converted += 50;
+                case 'C' -> converted += 100;
+                case 'D' -> converted += 500;
+                case 'M' -> converted += 1000;
+            }
+        }
+
+        return converted;
     }
 
     public void ImcResult() {
