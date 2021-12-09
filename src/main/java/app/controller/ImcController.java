bca0e4a7c77a1308e3d09b2e291d52583afcc39a
thibault <thibault.blanchard@edu.itescia.fr>
2021-12-09 09:20:16 +0100

    Optimisation et gestion des erreurs : - Message si character illegal - Seulement 3 digit pour Poids/taille - IMC : 2 chiffres après la virgule

--- src/main/java/app/controller/ImcController.java
+++ src/main/java/app/controller/ImcController.java
@@ -1,6 +1,8 @@
 package app.controller;
 
 import app.model.Converter;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -8,6 +10,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class ImcController implements Initializable {
@@ -32,46 +36,71 @@ public class ImcController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        /* creation arrayList pour decomposer ce qui est tapé puis supprimer lettres et remplacer , par . ou try catch*/
+        tfPoids.setOnKeyReleased(eventP -> {
+            if (tfPoids.getText().matches("[0-9.]*")) {
+                if (tfPoids.getText().length() > 3) tfPoids.deletePreviousChar();
+            }else {
+                System.out.println("Illegal character.");
+                //if (tfPoids.getText().length() > 3) tfPoids.;
+            }
+        });
+        tfTaille.setOnKeyReleased(eventT ->{
+            if (tfTaille.getText().matches("[0-9.]*")) {
+                if (tfTaille.getText().length() > 5) tfPoids.deletePreviousChar();
+            } else System.out.println("Illegal character.");
+
+        });
+
         btnValid.setOnMouseClicked(event ->{
             System.out.println(tfPoids.getText());
             System.out.println(tfTaille.getText());
 
+        /*Check si textfields sont vides */
 
-            if (tfTaille.getText().isEmpty()){
+            if (tfTaille.getText().isEmpty() || tfPoids.getText().isEmpty()) {
+                lblCom.setText("Veuillez entrer votre taille et/ou poids");
+            }
+          /* if (tfTaille.getText().isEmpty()){
                 lblCom.setText("Veuillez entrer votre taille");
 
             }
             if (tfPoids.getText().isEmpty()) {
                 lblCom.setText("Veuillez entrer votre poids");
 
-            }
-            converter.ImcResult(tfPoids.getText(), tfTaille.getText());
+            }*/
 
+            converter.ImcResult(tfPoids.getText(), tfTaille.getText());
+            float imcF = converter.ImcResult(tfPoids.getText(),tfTaille.getText());
+
+
+            /* Si NON Vide */
             if (!(tfPoids.getText().isEmpty() && tfTaille.getText().isEmpty())){
 
-                String Imc = Float.toString(converter.ImcResult(tfPoids.getText(), tfTaille.getText()));
-                tfImc.setText(Imc);
+                DecimalFormat df = new DecimalFormat("#######.##");
+                String imcS = df.format(imcF);
+                tfImc.setText(imcS);
 
-                if (converter.ImcResult(tfPoids.getText(), tfTaille.getText()) < 18.4){
+                if (imcF < 18.4){
                     lblCom.setText("Vous devez manger plus");
                 }
-                else if (converter.ImcResult(tfPoids.getText(), tfTaille.getText()) > 18.4 && converter.ImcResult(tfPoids.getText(), tfTaille.getText()) < 24.9){
+                else if (imcF > 18.4 && imcF < 24.9){
                     lblCom.setText("Votre corpulence est normale");
 
                 }
-                else if (converter.ImcResult(tfPoids.getText(), tfTaille.getText()) > 25 && converter.ImcResult(tfPoids.getText(), tfTaille.getText()) < 29.9){
+                else if (imcF > 25 && imcF < 29.9){
                     lblCom.setText("Vous etes en surpoids");
 
                 }
-                else if (converter.ImcResult(tfPoids.getText(), tfTaille.getText()) > 30 && converter.ImcResult(tfPoids.getText(), tfTaille.getText()) < 34.9){
+                else if (imcF > 30 && imcF < 34.9){
                     lblCom.setText("Votre obésité est modérée");
 
                 }
-                else if (converter.ImcResult(tfPoids.getText(), tfTaille.getText()) > 35 && converter.ImcResult(tfPoids.getText(), tfTaille.getText()) < 39.9){
+                else if (imcF > 35 && imcF < 39.9){
                     lblCom.setText("Votre obésité est sévère");
 
                 }
-                else if (converter.ImcResult(tfPoids.getText(), tfTaille.getText()) > 40){
+                else {
                     lblCom.setText("ATTENTION = Obésité morbide");
 
                 }
--- src/main/java/app/model/Converter.java
+++ src/main/java/app/model/Converter.java
@@ -1,5 +1,8 @@
 package app.model;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class Converter {
 
     public void DecToBin() {
@@ -21,12 +24,11 @@ public class Converter {
     }
 
     public Float ImcResult(String poids, String taille) {
+
         float p = Float.parseFloat(poids);
         float t = Float.parseFloat(taille);
 
         float imcF = p/(t*t);
-
-        //String imcS = Float.toString(imcF);
 
         return imcF;
 
